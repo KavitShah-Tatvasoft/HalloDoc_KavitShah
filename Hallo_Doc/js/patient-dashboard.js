ce4031d56f9fda98cc901cfe736d0f76470a3cb5
Kavit Shah <pca25@server7.com>
2024-02-20 19:23:25 +0530

    Added Admin SIde UI pages. The added pages are Add Business, Update Business, Provider Profile, Provider Menu, Account Access, Create Role(created 1 includes all 4)

--- Hallo_Doc/js/patient-dashboard.js
+++ Hallo_Doc/js/patient-dashboard.js
@@ -2,7 +2,8 @@ function changeCss(element){
     // console.log(element.id)
     var x = document.getElementById("me").style;
     var y = document.getElementById("else").style;
-    
+    var x_btn = document.getElementById("link-for-me").style;
+    var y_btn = document.getElementById("link-for-someone-else").style;
 
     if(element.id=="me"){
       x.backgroundColor = "#01bce9";
@@ -10,6 +11,9 @@ function changeCss(element){
 
       y.color = "#01bce9";
       y.backgroundColor = "white";
+
+      x_btn.display = "flex";
+      y_btn.display = "none";
     }
 
     else{
@@ -18,5 +22,8 @@ function changeCss(element){
 
       x.color = "#01bce9";
       x.backgroundColor = "white";
+
+      y_btn.display = "flex";
+      x_btn.display = "none";
     }
   }
